4941e29ea63fb3ff19a7919f1d1f367e2c43f98c
Antonio Estrada <yo.soy.quien.soy.xd@gmail.com>
2024-02-09 00:52:24 -0600

    CLI Advancements and API Post integration

--- Al.py
+++ Al.py
@@ -7,15 +7,14 @@ from datetime import datetime
 
 #IMPORTING MODULES
 from modules import netscan
-
-
-
+os.system('clear')
 # Global variables
 user = os.getenv('USER')
 ssh_client = os.getenv('SSH_CLIENT')
 system_info = os.uname()
 date_current = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 modules_directory = os.path.join(os.path.dirname(__file__),"modules")
+isStandby=True
 
 def script_setup(language,script_name):
     script_path = os.path.join(modules_directory,script_name)
@@ -29,7 +28,6 @@ def netscan():
     script_setup("python3","lan_scan/scan.py")
     
 def getSSHUser():
-    
     if ssh_client is None:
         print(f"Hello, {user}! My name is Al. Pleased to meet you.")
     else:
@@ -39,36 +37,46 @@ def getSSHUser():
         
         print(f"Hey {client_string}! My name is Al. Pleased to meet you.")
     
-    
-    #     try:
-    #         client_username= os.getenv('HOSTNAME')
     #         print(f"Hello {ssh_client}! Did you visit Elsweyr on your way here?")
-    #     except socket.herror:
-    #         print("Unable to resolve cliente username.")
     #         print(f"Hello {client_string}! Did you visit Elsweyr on your way here?")
 
 def hello():
-    os.system('clear')
+    global isStandby
     subprocess.run(["neofetch"])
     getSSHUser()
-    print(f"Your current session details:")
-    for info in system_info:
-        print(info)
+    print(f'You have successfully authenticated into the {system_info.nodename} network. Congratulations!')
+    print(f"The current time where you are located is time: {date_current}")
+    isStandby=modules(input("I'm still learning, but let me know if there is anything I can do to assist you.\n>>"))
+    
 
-    print(f"Time: {date_current}")
+def help():
+    global isStandby
+    print("|_")
+    print("The available modules as of right now are static. They are the following:")
+    print("netscan\nclear\nhello")
+    print("Help to get to this message.")
+    print("|_")
+    isStandby = modules(input("Which option do you choose?"))
+
 def clear_screen():
     os.system('clear')
 
 def idle():
+    global isStandby
     while True:
-        user_input=input("Can I help you with anything?\n>>")
-        if user_input.lower()=='n' or user_input.lower()=='no' or user_input.lower()=='exit':
-            print("It was a pleasure assisting you today. Have a wonderful day.")
-            break
-        modules(user_input.lower())
+        if isStandby:
+            isStandby =modules(input("Can I help you with anything?\n>>"))
+        else:
+            isStandby =modules(input(">>"))
 
    
-def modules(module):
+def modules(module) -> bool:
+    print("|_")
+    module= module.lower()
+    if module=='n' or module=='no' or module=='exit':
+            print("I appreciated our interaction today. I'll be here if you need me.")
+            return False  
+    
     if module == "netscan":
         netscan()
         return True
@@ -76,9 +84,12 @@ def modules(module):
         clear_screen()
     elif module == "hello":
         hello()
+    elif module == "help":
+        help()
     else:
-        print("Module not yet set. Please address Kaichou-sama with this.")
+        print("Module not yet set. Please address Kaichou-sama with this or try 'help' for available modules.")
         return False
+    return True
 
 if __name__ == "__main__":
     hello()
--- al-cli-v1.py
+++ al-cli-v1.py
@@ -8,56 +8,155 @@ from yaspin import yaspin
 import pyfiglet
 import datetime
 from tabulate import tabulate
+import configparser
+import os
 
-API_KEY="42c3cdf5ae2b1f81d78292c20b2eedf3"
 
-units = dict(
-  metric="°C",
-  imperial="°F"
-)
+ini = os.path.join(os.path.dirname(__file__),"program_data","api.ini")
+config = configparser.ConfigParser()
+config.read(ini)
+config_values= {}
 
-def weatherDataToTable(data, unit):
-  day=datetime.datetime.fromtimestamp(data['dt'])
-  return [f"{day:%d.%m.%Y}", minMaxTemp(data, unit)]
+for section in config.sections():
+  config_values[section]={}
+  for option in config.options(section):
+    config_values[section][option]= config.get(section,option)  
 
-def fetchWeeklyWeather(lon, lat, unit):
-  params = dict(
-    lon=lon,
-    lat=lat,
-    units= unit,
-    exclude="current,hourly,minutely,alerts",
-    appid= API_KEY
-  )
-  API_URL = "http://api.openweathermap.org/data/2.5/onecall"
-  response = requests.get(url=API_URL, params=params)
-  return response.json()["daily"]
+APIS={}
+for api in config_values['api_url']:
+  APIS[api]=config_values['api_url'][api]
 
-@yaspin(text="Fetching weather data...")
-def fetchWeather(city, country, unit):
-  params = dict(
-    q=f"{city},{country}",
-    units= unit,
-    appid= API_KEY
-  )
-  API_URL = "http://api.openweathermap.org/data/2.5/weather"
-  response = requests.get(url=API_URL, params=params)
-  lon=response.json()["coord"]["lon"]
-  lat=response.json()["coord"]["lat"]
-  return fetchWeeklyWeather(lon, lat, unit)
+APIKEYS={}
+for key in config_values['api_keys']:
+  APIKEYS[key]=config_values['api_keys'][key]
 
-def minMaxTemp(data, unit):
-  return "{}{} to {}{}".format(round(data["temp"]["min"]), units[unit], round(data["temp"]["max"]), units[unit])
+llm_config_data = {
+    'max_new_tokens': 128,
+    'min_length': 0,
+    'early_stopping': False,
+    'num_beams': 1,
+    'num_beam_groups': 1,
+    'use_cache': True,
+    'temperature': 0.75,
+    'top_k': 15,
+    'top_p': 0.78,
+    'typical_p': 1,
+    'epsilon_cutoff': 0,
+    'eta_cutoff': 0,
+    'diversity_penalty': 0,
+    'repetition_penalty': 1,
+    'encoder_repetition_penalty': 1,
+    'length_penalty': 1,
+    'no_repeat_ngram_size': 0,
+    'renormalize_logits': False,
+    'remove_invalid_values': False,
+    'num_return_sequences': 1,
+    'output_attentions': False,
+    'output_hidden_states': False,
+    'output_scores': False,
+    'encoder_no_repeat_ngram_size': 0,
+    'n': 1,
+    'presence_penalty': 0,
+    'frequency_penalty': 0,
+    'use_beam_search': False,
+    'ignore_eos': False,
+    'skip_special_tokens': True
+}
 
-def main(
-  city: str = typer.Option(..., prompt=True),
-  country: str = typer.Option(..., prompt=True)
-):
-  unit = inquirer.list_input("Metric or imperial?", choices=['metric', 'imperial'])
-  weatherData=fetchWeather(city, country, unit)
-  print(pyfiglet.figlet_format(minMaxTemp(weatherData[0], unit)))
-  tabledata = map(lambda x: weatherDataToTable(x, unit), weatherData)
-  headers = ["Date", "Temperature range"]
-  print(tabulate(tabledata, headers, tablefmt="fancy_grid"))
+
+
+def json_data(query,llm_config,adapter_name=None):
+  data = {
+        'prompt': query,
+        'stop': [],
+        'llm_config': llm_config,
+        'adapter_name': adapter_name, 
+    }
+  return json.dumps(data)
+
+
+
+def fetchToAPI(query,apiurl,apikey=None):
+
+  headers = {
+    'Content-Type':'application/json',
+    }
+  
+  
+#   headers = {
+#     # Already added when you pass json=
+#     # 'Content-Type': 'application/json',
+# }
+
+  json_data = {
+      'prompt': query,
+      'stop': [],
+      'llm_config': {
+          'max_new_tokens': 128,
+          'min_length': 0,
+          'early_stopping': False,
+          'num_beams': 1,
+          'num_beam_groups': 1,
+          'use_cache': True,
+          'temperature': 0.75,
+          'top_k': 15,
+          'top_p': 0.78,
+          'typical_p': 1,
+          'epsilon_cutoff': 0,
+          'eta_cutoff': 0,
+          'diversity_penalty': 0,
+          'repetition_penalty': 1,
+          'encoder_repetition_penalty': 1,
+          'length_penalty': 1,
+          'no_repeat_ngram_size': 0,
+          'renormalize_logits': False,
+          'remove_invalid_values': False,
+          'num_return_sequences': 1,
+          'output_attentions': False,
+          'output_hidden_states': False,
+          'output_scores': False,
+          'encoder_no_repeat_ngram_size': 0,
+          'n': 1,
+          'presence_penalty': 0,
+          'frequency_penalty': 0,
+          'use_beam_search': False,
+          'ignore_eos': False,
+          'skip_special_tokens': True,
+      },
+      'adapter_name': None,
+  }
+
+#   response = requests.post('http://localhost:3000/v1/generate', headers=headers, json=json_data)
+  response=requests.post(apiurl,headers=headers,json=json_data)
+  return response
+  
+
+def main():
+  while True:
+    API = inquirer.list_input("What api do you choose?",choices=APIS)
+    api_url = APIS[API]
+    api_key = APIKEYS[API]
+    
+    if api_key is None or api_key == '':
+      api_key=None
+    query = input("How may I help you today?\n>>>")
+    jason = json_data(query,llm_config_data)
+    responseData=fetchToAPI(query,api_url,api_key)
+    if responseData.status_code == 200:
+        print('POST request was successful!')
+        print('Response:', responseData.json())
+    else:
+        print('POST request failed with status code:', responseData.status_code)
+    
+    print("Restarting...")
+
+
+  
+  
+  
+  
+  
 
 if __name__ == "__main__":
     typer.run(main)
+    pass
