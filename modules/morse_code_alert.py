acce7f06851622e38ac3e5da2adb8ace2878a7f3
Al <al@kuraitachi.com>
2024-03-24 21:01:23 -0600

    AL-KOMCAS integration

--- modules/morse_code_alert.py
+++ modules/morse_code_alert.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import serial
 import time
 
@@ -5,12 +7,15 @@ arduino = serial.Serial(port='/dev/ttyACM0',baudrate=115200, timeout=1)
 
 def write_read(x):
     arduino.write(bytes(x, 'utf-8'))
-    
-    data = arduino.readline()
-    
-    return data
+    while True:
+        data = arduino.readline().decode().strip()
+        if '/420' in data:
+            break
+        print(data)
 
 while True:
-    num = input("Enter a message: ")
-    value = write_read(num)
-    print(value)
+    message = input("Enter a message: ")
+    value = write_read(message)
+    print("Message sent successfully. Restarting...")
+
+    
